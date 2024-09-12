841594a1cc6eb296d52e2f631d6986449df4d85f
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-09-13 01:57:36 +0300

    usign flag to enable debug logging

--- cmd/wordgen/tui.go
+++ cmd/wordgen/tui.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"wordgen/internal/tui"
 
@@ -8,11 +9,17 @@ import (
 )
 
 func main() {
-	f, err := tea.LogToFile("debug.log", "debug")
-	if err != nil {
-		log.Fatalf("ERRO: %v", err)
+	debug := false
+	flag.BoolVar(&debug, "debug", debug, "Enable debug logging")
+	flag.Parse()
+
+	if debug {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			log.Fatalf("ERRO: %v", err)
+		}
+		defer f.Close()
 	}
-	defer f.Close()
 
 	p := tea.NewProgram(tui.NewModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
